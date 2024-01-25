0e363afdd732fc865bd9950513e99e3cea10ae71
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-01-25 20:26:55 +0530

    first commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,48 @@
+import { useState, useCallback } from "react"
+
+import QUESTIONS from '../question'
+import quizCompleteImg from '../assets/quiz-complete.png'
+import Question from "./Question";
+
+export default function Quiz(){
+    
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const activeQuestionIndex = userAnswers.length;
+    
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+
+   const handleSelectAnswer = useCallback( function handleSelectAnswer(selectedAnswer){
+        // setAnswerState('answered')
+        setUserAnswers((prevUserAnswers) => {
+            return[...prevUserAnswers, selectedAnswer];
+        });
+
+        
+    },
+     []);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
+
+    
+    if (quizIsComplete){
+        return <div id="summary">
+            <img src={quizCompleteImg} alt="Trophy Icon" />
+            <h2>Quiz Completed !</h2>
+        </div>
+    }
+
+
+   
+    return( 
+    <div id="quiz">
+        <Question 
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+        />   
+</div>
+
+);
+}
